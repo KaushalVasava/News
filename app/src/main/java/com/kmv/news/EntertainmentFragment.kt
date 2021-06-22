5d797c3924de71c2d5b967362385c74954d5aa73
KauhsalVasava <vasavakaushal333@gmail.com>
2021-06-22 18:04:40 +0530

    Bottom navigation Added

---
+++ app/src/main/java/com/kmv/news/EntertainmentFragment.kt
@@ -0,0 +1,99 @@
+package com.kmv.news
+
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.kmv.news.MySingletone
+import com.kmv.news.News
+import com.kmv.news.NewsAdapter
+import com.kmv.news.NewsItemListener
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class EntertainmentFragment : Fragment(), NewsItemListener {
+
+    lateinit var recyclerview2 : RecyclerView
+
+    lateinit var madapter: NewsAdapter
+
+    public override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): android.view.View? {
+        val view : View = inflater.inflate(R.layout.fragment_entertainment, container, false);
+
+        recyclerview2= view.findViewById(R.id.recyclerview2)
+        recyclerview2.layoutManager = LinearLayoutManager(view.context)
+        madapter = NewsAdapter(this)
+        recyclerview2.adapter = madapter
+        fetchdata()
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
+    private fun fetchdata(){
+        //val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e7b862bc8d7c46fb809cec353fed07d9"
+        val url = "https://saurav.tech/NewsAPI/top-headlines/category/entertainment/in.json"
+        val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET, url,null,
+                Response.Listener{
+            val newsJsonArray = it.getJSONArray("articles")
+            val newsArray = ArrayList<News>()
+            for(i in 0 until newsJsonArray.length()){
+                val newsObject = newsJsonArray.getJSONObject(i)
+                val news = News(
+                        newsObject.getString("author"),
+                        newsObject.getString("title"),
+                        newsObject.getString("url"),
+                        newsObject.getString("urlToImage")
+                )
+
+                val s = newsObject.getString("title")
+
+                val last = s.length-1
+                var index = 0
+                for (i in last downTo 0) {
+                    if (s[i] == '-') {
+                        index = i
+                        break
+                    }
+                }
+                val sub = s.substring(index+1, last+1)
+                news.author=sub
+                newsArray.add(news)
+            }
+            madapter.updateNews(newsArray)
+        },
+        {
+            val toast = Toast.makeText(context, "No Internet Connection", Toast.LENGTH_LONG)
+            toast.show()
+        })
+        context?.let { MySingletone.getInstance(it).addToRequestQueue(jsonObjectRequest) }
+    }
+    override fun onItemClicked(item: News) {
+        val colorInt: Int = Color.parseColor("#FFFFFF") //blue
+
+        //    Toast.makeText(this,"$item clicked",Toast.LENGTH_SHORT).show()
+        val builder = CustomTabsIntent.Builder();
+        builder.setToolbarColor(colorInt)
+        val customTabsIntent= builder.build();
+        context?.let { customTabsIntent.launchUrl(it, Uri.parse(item.url)) };
+    }
+}
---
+++ app/src/main/java/com/kmv/news/TechnologyFragment.kt
@@ -0,0 +1,99 @@
+package com.kmv.news
+
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.kmv.news.MySingletone
+import com.kmv.news.News
+import com.kmv.news.NewsAdapter
+import com.kmv.news.NewsItemListener
+
+
+/**
+ * A simple {@link Fragment} subclass.
+
+ * create an instance of this fragment.
+ */
+public class TechnologyFragment : Fragment(), NewsItemListener {
+
+    lateinit var recyclerview4 : RecyclerView
+
+    lateinit var madapter: NewsAdapter
+
+    public override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): android.view.View? {
+        val view : View = inflater.inflate(R.layout.fragment_technology, container, false);
+
+        recyclerview4= view.findViewById(R.id.recyclerview4)
+        recyclerview4.layoutManager = LinearLayoutManager(view.context)
+        madapter = NewsAdapter(this)
+        recyclerview4.adapter = madapter
+        fetchdata()
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
+    private fun fetchdata(){
+        //val url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e7b862bc8d7c46fb809cec353fed07d9"
+        val url = "https://saurav.tech/NewsAPI/top-headlines/category/technology/in.json"
+        val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET, url,null,
+                Response.Listener{
+            val newsJsonArray = it.getJSONArray("articles")
+            val newsArray = ArrayList<News>()
+            for(i in 0 until newsJsonArray.length()){
+                val newsObject = newsJsonArray.getJSONObject(i)
+                val news = News(
+                        newsObject.getString("author"),
+                        newsObject.getString("title"),
+                        newsObject.getString("url"),
+                        newsObject.getString("urlToImage")
+                )
+                val s = newsObject.getString("title")
+
+                val last = s.length-1
+                var index = 0
+                for (i in last downTo 0) {
+                    if (s[i] == '-') {
+                        index = i
+                        break
+                    }
+                }
+                val sub = s.substring(index+1, last+1)
+                news.author=sub
+                newsArray.add(news)
+            }
+            madapter.updateNews(newsArray)
+        },
+        {
+            val toast = Toast.makeText(context, "No Internet Connection", Toast.LENGTH_LONG)
+            toast.show()
+        })
+        context?.let { MySingletone.getInstance(it).addToRequestQueue(jsonObjectRequest) }
+    }
+    override fun onItemClicked(item: News) {
+        val colorInt: Int = Color.parseColor("#FFFFFF") //blue
+
+        //    Toast.makeText(this,"$item clicked",Toast.LENGTH_SHORT).show()
+        val builder = CustomTabsIntent.Builder();
+        builder.setToolbarColor(colorInt)
+        val customTabsIntent= builder.build();
+        context?.let { customTabsIntent.launchUrl(it, Uri.parse(item.url)) };
+    }
+}
